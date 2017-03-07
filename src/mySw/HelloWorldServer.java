a9f8bf9ea447cd5e3b9184fbb04fe249de671cc5
rafiq <karim.abderrafiq@gmail.com>
2017-03-07 15:31:32 +0100

    nothing special

--- src/mySw/HelloWorldServer.java
+++ src/mySw/HelloWorldServer.java
@@ -1,5 +1,14 @@
 package mySw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +20,34 @@ public class HelloWorldServer {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String sayHello(){
+		System.out.println("je ss appelé");
 		return "hello";
 	}
-}
+	
+	/*@GET
+	@Produces(MediaType.TEXT_HTML)
+	public void process() throws IOException{
+		int port = 2017;
+	    ServerSocket ss = new ServerSocket(port);
+	    for (;;) {
+	        Socket client = ss.accept();
+	        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+	        PrintWriter out = new PrintWriter(client.getOutputStream());
+	        out.print("ok");
+	        
+	        String line;
+	        while ((line = in.readLine()) != null) {
+	            if (line.length() == 0)
+	              break;
+	            out.print(line + "\r\n");
+	          }
+	          out.close(); 
+	          in.close(); 
+	          client.close();
+	        } 
+	        }
+	    */
+
+		
+	}
+
